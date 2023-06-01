b903cf83848eccd124fa18e3b0992a0498eb8a20
dbquiroga <day@xcapit.com>
2023-06-01 19:20:07 -0300

    primer POM

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,11 @@
 /// <reference types="cypress" />
+import { login } from "../support/Pages/loginPage";
 
-describe ('Test del login', () => {
-    it('Login exitoso', () => {
-        cy.visit('/')
-        cy.get('#user-name').type(`standard_user`)
-        cy.get('#password').type(`secret_sauce`)
-        cy.get('#login-button').click()
-    });
+describe("Test del login", () => {
+  it("Login exitoso", () => {
+    cy.visit("/");
+    cy.get(login.loginInput).type(`standard_user`);
+    cy.get("#password").type(`secret_sauce`);
+    cy.get("#login-button").click();
+  });
 });
---
+++ cypress/support/Pages/loginPage.js
@@ -0,0 +1,12 @@
+class Login {
+  constructor() {
+    this.loginInput = "#user-name";
+    this.passInput = "#password";
+  }
+
+  login(username, password) {
+    // aca iria las instrucciones
+  }
+}
+
+export const login = new Login();
