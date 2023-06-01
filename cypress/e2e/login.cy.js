6bcebd92b64eaea3f292ea888f8d7e0465697fb0
CarolinaEMedinaArg <carolinaestefaniamedina@gmail.com>
2023-06-01 19:58:53 -0300

    POM of products created & making assertions of login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,13 @@
 /// <reference types="cypress" />
 import { login } from "../support/Pages/loginPage";
-
+import { productsPage } from "../support/Pages/inventory";
 describe("Test del login", () => {
   it("Login exitoso", () => {
     cy.visit("/");
     cy.get(login.loginInput).type(`standard_user`);
-    cy.get("#password").type(`secret_sauce`);
-    cy.get("#login-button").click();
+    cy.get(login.passInput).type(`secret_sauce`);
+    cy.get(login.loginBtn).click();
+    cy.url().should("include", "/inventory.html");
+    cy.get(productsPage.productsContainer).should("exist");
   });
 });
--- cypress/support/Pages/loginPage.js
+++ cypress/support/Pages/loginPage.js
@@ -2,6 +2,7 @@ class Login {
   constructor() {
     this.loginInput = "#user-name";
     this.passInput = "#password";
+    this.loginBtn = "#login-button";
   }
 
   login(username, password) {
