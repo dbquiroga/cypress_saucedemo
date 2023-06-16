479f7674735158693a3dc8d1041004d7f4ad0784
dbquiroga <day@xcapit.com>
2023-06-15 21:20:10 -0300

    agregando el primer fixture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 import { login } from "../support/Pages/loginPage";
 import { productsPage } from "../support/Pages/inventory";
+import { sucess_user } from "../fixtures/usuarios.json";
+
 describe("Test del login", () => {
   it("Login exitoso", () => {
     cy.visit("/");
-    login.login("standard_user", "secret_sauce");
+    login.login(sucess_user.usuario, sucess_user.password);
     cy.url().should("include", "/inventory.html");
     cy.get(productsPage.productsContainer).should("exist");
   });
