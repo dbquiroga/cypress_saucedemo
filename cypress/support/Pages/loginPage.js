ee1618112b253b3b059fae3a477780285d68de07
Karina Hanmse <karinahanmse@gmail.com>
2023-06-22 20:26:45 -0300

    Usuario Inexistente Escenario 3

--- cypress/support/Pages/loginPage.js
+++ cypress/support/Pages/loginPage.js
@@ -3,10 +3,11 @@ class Login {
     this.loginInput = "#user-name";
     this.passInput = "#password";
     this.loginBtn = "#login-button";
+    this.errorMessage = ".error-message-container h3";
   }
 
   login(username, password) {
-    // aca iria las instrucciones
+    // aca iría las instrucciones
     cy.get(this.loginInput).type(username);
     cy.get(this.passInput).type(password);
     cy.get(this.loginBtn).click();
